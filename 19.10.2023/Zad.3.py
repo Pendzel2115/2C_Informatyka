fbb4c777cd3b4cf3c41d8bcbfe9cac1d893b0d82
Igor <146358447+Pendzel2115@users.noreply.github.com>
2023-10-26 16:29:10 +0200

    Zadania

---
+++ 19.10.2023/Zad.3.py
@@ -0,0 +1,5 @@
+# Program wyświetli w pierwszym wierszu dwa pierwsze owoce a pod spodem kiwi
+print("gruszka", end=" ")
+print("banan")
+print("kiwi")
+
---
+++ 19.10.2023/zad1.py
@@ -0,0 +1,3 @@
+print("Mój pierwszy program w języku Python")
+
+
